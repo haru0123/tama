002106a0adb2cac1f0f53716064d6832348dc9c0
kadowaki <jmn2012km@gmail.com>
2024-11-02 19:00:35 +0900

    java setup

---
+++ src/main/java/com/example/demo/FinancialQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialQuizApplication.class, args);
+	}
+
+}
